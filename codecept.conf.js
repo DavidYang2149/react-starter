c104eed242371b59665296615688c0d2b7a92293
davidyang2149 <davidyang2149@gmail.com>
2022-05-29 20:35:15 +0900

    CodeceptJS 설정

---
+++ codecept.conf.js
@@ -0,0 +1,23 @@
+const { setHeadlessWhen, setCommonPlugins } = require('@codeceptjs/configure');
+
+// turn on headless mode when running with HEADLESS=true environment variable
+// export HEADLESS=true && npx codeceptjs run
+setHeadlessWhen(process.env.HEADLESS);
+
+// enable all common plugins https://github.com/codeceptjs/configure#setcommonplugins
+setCommonPlugins();
+
+exports.config = {
+  tests: './tests/**/*_test.ts',
+  output: './output',
+  helpers: {
+    Playwright: {
+      url: 'http://localhost:8080',
+      show: true,
+      browser: 'chromium',
+    },
+  },
+  bootstrap: null,
+  mocha: {},
+  name: 'react-starter',
+};
