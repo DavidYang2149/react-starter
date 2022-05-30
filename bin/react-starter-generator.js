e75e190d3b62e2fa3e15890564aa0755f27a3c69
davidyang2149 <davidyang2149@gmail.com>
2022-05-30 15:25:21 +0900

    프로젝트 설치 스크립트 제작

---
+++ bin/react-starter-generator.js
@@ -0,0 +1,101 @@
+#! /usr/bin/env node
+
+const { execSync } = require('child_process');
+const path = require('path');
+const fs = require('fs');
+
+const { log } = console;
+
+if (process.argv.length < 3) {
+  log('\x1b[31m%s\x1b[0m', '========================= 🚨 E R R O R 🚨 =========================');
+  log('\x1b[31m%s\x1b[0m', '🚨 Warning:');
+  log('');
+  log('Please specify the project directory:');
+  log('\x1b[32m%s\x1b[0m', '@davidyang2149/react-starter <project-directory>');
+  log('');
+  log('For example:');
+  log('npx @davidyang2149/react-starter my-react-project');
+  log('\x1b[31m%s\x1b[0m', '=========================== 🚨 E N D 🚨 ===========================');
+
+  process.exit(1);
+}
+
+const projectName = process.argv[2];
+const currentPath = process.cwd();
+const projectPath = path.join(currentPath, projectName);
+const GIT_REPOSITORY = 'https://github.com/DavidYang2149/react-starter';
+
+const gitCloneOrderArray = ['git', 'clone', '--depth', '1', GIT_REPOSITORY, projectPath];
+const gitCloneCommand = gitCloneOrderArray.map((item) => item.replace(/\s/g, '')).join(' ');
+
+if (projectName !== '.') {
+  try {
+    fs.mkdirSync(projectPath);
+  } catch (err) {
+    if (err.code === 'EEXIST') {
+      log('\x1b[31m%s\x1b[0m', '========================= 🚨 E R R O R 🚨 =========================');
+      log(projectName);
+      log(
+        '\x1b[31m%s\x1b[0m',
+        `🚨 The folder ${projectName} already exist in the current directory, please give it another name.`,
+      );
+      log('\x1b[31m%s\x1b[0m', '=========================== 🚨 E N D 🚨 ===========================');
+    } else {
+      log('\x1b[31m%s\x1b[0m', '========================= 🚨 E R R O R 🚨 =========================');
+      log(err);
+      log('\x1b[31m%s\x1b[0m', '=========================== 🚨 E N D 🚨 ===========================');
+    }
+    process.exit(1);
+  }
+}
+
+async function main() {
+  try {
+    log('========================= 🚀 S T A R T 🚀 =========================');
+    log('Using npm.');
+    log('');
+    log('Installing dependencies:');
+    log('\x1b[32m%s\x1b[0m', '- react');
+    log('\x1b[32m%s\x1b[0m', '- react-dom');
+    log('');
+    log('Installing devDependencies:');
+    log('\x1b[32m%s\x1b[0m', '- typescript');
+    log('\x1b[32m%s\x1b[0m', '- eslint');
+    log('\x1b[32m%s\x1b[0m', '- jest');
+    log('\x1b[32m%s\x1b[0m', '- testing-library');
+    log('\x1b[32m%s\x1b[0m', '- codeceptjs');
+    log('');
+    log('⭐ React Starter Kit ⭐');
+    log('🥰 Create by davidyang2149');
+    log('🚀 From https://github.com/DavidYang2149/react-starter');
+    log('');
+    log(`🚀 Creating project ${projectName}...`);
+    log('');
+    log('🚚 Downloading files:');
+    execSync(gitCloneCommand);
+
+    if (projectName !== '.') {
+      process.chdir(projectPath);
+    }
+
+    log('');
+    log('📦 Installing dependencies:');
+    execSync('npm install');
+    log('');
+    log('🔥 Removing useless files:');
+    execSync('npx rimraf ./.git');
+    log('');
+    execSync(`npx rimraf ${projectPath}/bin`);
+    log('');
+    log('\x1b[36m%s\x1b[0m', 'Successfully installed!');
+    log('');
+    log('\x1b[35m%s\x1b[0m', '🎉 The installation is done, ready to use. Happy coding!');
+    log('========================= 🎉 E N D 🎉 =========================');
+  } catch (error) {
+    log('\x1b[31m%s\x1b[0m', '========================= 🚨 E R R O R 🚨 =========================');
+    log(error);
+    log('\x1b[31m%s\x1b[0m', '=========================== 🚨 E N D 🚨 ===========================');
+  }
+}
+
+main();
