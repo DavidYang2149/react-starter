cd9497cd4aed4c5b3531d7a62ffd48b376660b6a
davidyang2149 <davidyang2149@gmail.com>
2022-07-15 18:54:17 +0900

    ReactDOM 리액트 18 버전 대응

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import * as ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 import App from './App';
 
 const container = document.getElementById('app');
-ReactDOM.render(<App />, container);
+const root = createRoot(container!);
+root.render(<App />);
