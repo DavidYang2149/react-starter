417b80f4a212e88f6af5bf2d60405f08c41b917d
Yang Hong Seok <davidyang2149@gmail.com>
2022-09-02 11:30:14 +0900

    :wrench: E2E 보고서 환경설정을 작성합니다

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -35,3 +35,16 @@
 //     }
 //   }
 // }
+
+/* eslint-disable import/no-extraneous-dependencies */
+import addContext from 'mochawesome/addContext';
+import Mocha from 'mocha';
+
+Cypress.on('test:after:run', (test: Mocha.Runnable, runnable) => {
+  let videoName = Cypress.spec.name;
+  videoName = videoName.replace('/.js.*', '.js');
+  const videoUrl = `videos/${videoName}.mp4`;
+  const context = { test } as Mocha.Context;
+
+  addContext(context, videoUrl);
+});
